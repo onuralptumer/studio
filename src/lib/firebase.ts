864f98057e6386fac3cd92a7ff102d2da1dac462
onuralp tümer <onuralptumer@gmail.com>
2025-09-17 19:04:22 +0000

    eventhough I enabled google I get authentication error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyAlXrZEk4CcCOeYKHiSEF1SBrKlcL4vEyY",
     authDomain: "studio-6709895403-e0278.firebaseapp.com",
     projectId: "studio-6709895403-e0278",
-    storageBucket: "studio-6709895403-e0278.firebasestorage.app",
+    storageBucket: "studio-6709895403-e0278.appspot.com",
     messagingSenderId: "560846416562",
     appId: "1:560846416562:web:7f5411bc40bde29fd8f7ab"
   };
