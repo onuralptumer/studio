b2b4b42945314a04269ac31377ee28012b5e8b6f
onuralp tümer <onuralptumer@gmail.com>
2025-09-18 05:59:39 +0000

    in the network section I see this error:
    Potential permissions policy v

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,23 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
