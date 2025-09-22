f21eb6f49d427d47f7d35a3ca777d0247bea849a
onuralp tümer <onuralptumer@gmail.com>
2025-09-22 22:00:57 +0000

    key change, timer fix

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,12 +10,12 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlXrZEk4CcCOeYKHiSEF1SBrKlcL4vEyY",
-  authDomain: "studio-6709895403-e0278.firebaseapp.com",
-  projectId: "studio-6709895403-e0278",
-  storageBucket: "studio-6709895403-e0278.firebasestorage.app",
-  messagingSenderId: "560846416562",
-  appId: "1:560846416562:web:7f5411bc40bde29fd8f7ab"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
